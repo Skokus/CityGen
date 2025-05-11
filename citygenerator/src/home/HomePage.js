045ab097a05df8b6e6e79a41bdec4114a25737f7
Paweł Cegielski <skok2022@gmail.com>
2025-05-11 23:36:17 +0200

    Adding HomePage and d3 react to project
    https://react-d3-library.github.io/

---
+++ citygenerator/src/home/HomePage.js
@@ -0,0 +1,11 @@
+import styles from "./HomePage.css"
+
+function HomePage() {
+  return (
+    <div>
+      <div className="title">Medieval City Generator</div>
+    </div>
+  );
+}
+
+export default HomePage;
