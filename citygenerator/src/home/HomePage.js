c79c3ab28802e7b97f93cfd6713b93205110f646
Paweł Cegielski <skok2022@gmail.com>
2025-05-12 18:29:16 +0200

    Flexbox in Homepage, defining WorldEvent

--- citygenerator/src/home/HomePage.js
+++ citygenerator/src/home/HomePage.js
@@ -1,9 +1,13 @@
-import styles from "./HomePage.css"
+import "./HomePage.css"
 
 function HomePage() {
   return (
     <div>
       <div className="title">Medieval City Generator</div>
+      <div className="container">
+        <div className="mainpanel">Main</div>
+        <div className="sidebar">Sidebar</div>
+      </div>
     </div>
   );
 }
---
+++ citygenerator/src/worldevent/models/WorldEvent.js
@@ -0,0 +1,8 @@
+class WorldEvent {
+
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+  }
+  
+}
