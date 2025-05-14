972f54f195e743f0811416b7dd82497cb6b0a497
Paweł Cegielski <skok2022@gmail.com>
2025-05-14 22:15:41 +0200

    First try with d3 voronoi

---
+++ citygenerator/src/citymap/models/City.js
@@ -0,0 +1,39 @@
+import { Delaunay } from "d3";
+import { useRef, useMemo, useEffect } from "react";
+
+function City() {
+
+  const canvasRef = useRef(null);
+  var w = 1000;
+  var h = 1000;
+  const data = Array(100)
+      .fill()
+      .map((_, i) => ({ x: (i * w) / 100, y: Math.random() * h }));
+
+  const delaunay = useMemo(() => {
+    const formattedData = data.map((d) => [d.x, d.y]);
+    return Delaunay.from(formattedData);
+  }, []);
+
+  const voronoi = useMemo(() => {
+    return delaunay.voronoi([0,0,1000,1000]);
+  }, [data]);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    const segments = voronoi.render().split(/M/).slice(1);
+    for (const e of segments) {
+      ctx.beginPath();
+      ctx.stroke(new Path2D("M" + e));
+    }
+  }, []);
+  
+  return (
+    <div className="container">
+      <canvas ref={canvasRef} width={1000} height={1000}/>
+    </div>
+  );
+}
+
+export default City;
--- citygenerator/src/home/HomePage.js
+++ citygenerator/src/home/HomePage.js
@@ -1,15 +1,19 @@
+import City from "../citymap/models/City";
 import WorldEventPanel from "../worldevent/components/WorldEventPanel";
 import WorldEvent from "../worldevent/models/WorldEvent";
 import "./HomePage.css"
 
 function HomePage() {
+  
   return (
     <div>
       <div className="title">Medieval City Generator</div>
       <div className="container">
-        <div className="mainpanel">Main</div>
-        <div className="sidepanel">
-          <WorldEventPanel  worldevents={WorldEvent.getExampleWorldEvents()}/>
+        <div className="mainpanel">
+          <City/>
+        </div>
+        <div classame="sidepanel">
+          <WorldEventPanel worldevents={WorldEvent.getExampleWorldEvents()}/>
         </div>
       </div>
     </div>
