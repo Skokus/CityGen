b7e62dfa0eace621968023a6c60cf0d058ffef4e
Paweł Cegielski <skok2022@gmail.com>
2025-11-25 13:30:25 +0100

    Different colors for building

--- citygenerator/src/citymap/models/building/polygonbuilding/HousingPBuilding.ts
+++ citygenerator/src/citymap/models/building/polygonbuilding/HousingPBuilding.ts
@@ -6,7 +6,13 @@ import Road from "../../road/Road";
 class HousingPBuilding extends PolygonBuilding {
 
     protected getDefaultColor(): string {
-        return "#ff9a02";
+        const c = Math.random();
+        if(c < 0.33){
+            return "#a16300";
+        } else if (c < 0.66){
+            return "#c37c00";
+        }
+        return "#834f00";
     }
 
     //ratio of smaller polygon to bigger one (borders)
